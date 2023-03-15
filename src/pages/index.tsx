f29ce483c6ede6a9cf9bcdea7dd700d906d664d0
Gavin Sidhu <gavinjeet.sidhu@gmail.com>
2023-03-14 22:20:59 -0700

    made font size smaller

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,18 +21,18 @@ export default function Home() {
         <Container>
           <div className='flexRow'>
           <Card className='cardLg'>
-              <h1 className='displayHeading'>Hi, I'm Gavin, a self-taught full stack developer.</h1>
+              <h1 className='displayHeading'>Hi, I'm Gavin, a fullstack developer based in California.</h1>
               <div className='lineBreak'></div>
-              <p>I love tackling complex problems and finding creative solutions. Whether it's a small feature or a full-fledged application, I enjoy the challenge of making it work seamlessly.</p>
-              <p>As a self-taught developer, I've learned how to work independently, solve complex problems, and continuously improve my skills through hands-on experience and self-study. I believe that technology has the power to make a positive impact on people's lives, and I'm always looking for new and exciting ways to create solutions that make a difference.</p>
-              <p>In my free time, you can find me tinkering with new technologies, reading about the latest trends in web development, or working on personal projects that challenge me to learn and grow. Whether it's building a website from scratch or optimizing a complex application, I always strive to deliver high-quality results that exceed expectations.</p>
+              <p>I enjoy coming up interesting solutions for tough problems.</p>
+              <p>Whether it's a tiny feature or a full-blown app, I thrive on the challenge of making it work smoothly.</p>
+              <p>I believe that technology has the power to make a positive impact on people's lives, and I'm always looking for new and exciting ways to create solutions that make a difference.</p>
             </Card>
           </div>
           <div className='homeGrid'>
             <Card className='cardMd cardLink' link="https://www.storystarters.co" >
               <div>
               <h1 className='displayHeading'>Story Starters</h1>
-              <p>Description of project</p>
+              <p>AI story writer</p>
               <div className='lineBreak'></div>
               <div className='projectImage'>
                 <img
@@ -46,7 +46,7 @@ export default function Home() {
             <Card className='cardMd cardLink' link="https://www.optibot.io">
               <div>
               <h1 className='displayHeading'>Optibot</h1>
-              <p>Description of project</p>
+              <p>VS Code extention that helps optimize code</p>
               <div className='lineBreak'></div>
               <div className='projectImage'>
                 <img
